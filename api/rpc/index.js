821640b1c6d89311dc90e88bd90ca1bdab771075
lanchongyizu <lanchongyizu@gmail.com>
2017-05-05 02:20:28 -0400

    separate task and graph

---
+++ api/rpc/index.js
@@ -0,0 +1,94 @@
+// Copyright © 2017 Dell Inc. or its subsidiaries. All Rights Reserved
+'use strict';
+
+var di = require('di');
+
+module.exports = schedulerServerFactory;
+di.annotate(schedulerServerFactory, new di.Provide('TaskGraph.TaskScheduler.Server'));
+di.annotate(schedulerServerFactory,
+    new di.Inject(
+        'Assert',
+        '_',
+        'Promise',
+        'Logger'
+    )
+);
+
+function schedulerServerFactory(
+    assert,
+    _,
+    Promise,
+    Logger
+) {
+
+    var logger = Logger.initialize(schedulerServerFactory);
+
+    function SchedulerServer(options) {
+        this.options = options || {
+            hostname: '0.0.0.0'
+        };
+        this.options.protoFile = this.options.protoFile || __dirname + '/../../scheduler.proto';
+
+        assert.string(this.options.hostname);
+        assert.string(this.options.protoFile);
+    }
+
+    SchedulerServer.prototype.start = function() {
+        var self = this;
+
+        return Promise.try(function() {
+            var grpc = require('grpc');
+            var schedulerProto = grpc.load(self.options.protoFile).scheduler;
+
+            var tasks = require('./tasks.js');
+            var workflowTasks = require('./workflowTasks.js');
+
+            self.gRPC = new grpc.Server();
+            self.gRPC.addProtoService(schedulerProto.Scheduler.service, {
+                getBootstrap: grpcWrapper(tasks.getBootstrap),
+                getTasksById: grpcWrapper(tasks.getTasksById),
+                postTaskById: grpcWrapper(tasks.postTaskById),
+                workflowsPutTask: grpcWrapper(workflowTasks.workflowsPutTask),
+                workflowsGetAllTasks: grpcWrapper(workflowTasks.workflowsGetAllTasks),
+                workflowsGetTasksByName: grpcWrapper(workflowTasks.workflowsGetTasksByName),
+                workflowsDeleteTasksByName: grpcWrapper(workflowTasks.workflowsDeleteTasksByName)
+            });
+
+            self.options.port = self.gRPC.bind(
+                self.options.hostname + (self.options.port ? ':' + self.options.port : ''),
+                grpc.ServerCredentials.createInsecure());
+            self.gRPC.start();
+            logger.info('gRPC is available on grpc://' +
+                (self.options.port ? self.options.hostname + ':' + self.options.port : self.options.hostname));
+        });
+    };
+
+    SchedulerServer.prototype.stop = function() {
+        var self = this;
+        return Promise.try(function() {
+            self.gRPC.forceShutdown();
+        });
+    };
+
+    function grpcWrapper(rpcEntry) {
+        return function(call, callback) {
+            return Promise.try(function() {
+                return rpcEntry(call, callback);
+            })
+            .then(function(response) {
+                if(response) {
+                    callback(null, {
+                        response: JSON.stringify(response)
+                    });
+                }
+            })
+            .catch(function(err) {
+                callback(err);
+            });
+        };
+    }
+
+    return SchedulerServer;
+}
+
+
--- index.js
+++ index.js
@@ -4,11 +4,13 @@
 
 var di = require('di'),
     _ = require('lodash'),
+    consul = require('consul'),
     core = require('on-core')(di);
 var temp = require('temp');
 
-module.exports = {
-    injectables: _.flattenDeep([
+var injectables =  _.flattenDeep([
+        core.injectables,
+        core.workflowInjectables,
         core.helper.requireWrapper('ssh2', 'ssh', undefined, __dirname),
         core.helper.requireWrapper('dockerode', 'Docker', undefined, __dirname),
         core.helper.simpleWrapper(temp, 'temp'),
@@ -20,7 +22,109 @@ module.exports = {
         core.helper.simpleWrapper(
             core.helper.requireGlob(__dirname + '/lib/task-data/**/*.js'),
             'Task.taskLibrary'
-        )
-    ]),
-    taskSchemas: core.helper.requireGlob(__dirname + '/lib/task-data/schemas/*.json')
+        ),
+        core.helper.requireGlob(__dirname + '/api/rest/view/**/*.js'),
+        require('./api/rpc/index.js'),
+        core.helper.simpleWrapper(consul, 'consul')
+    ]);
+
+var taskSchemas = core.helper.requireGlob(__dirname + '/lib/task-data/schemas/*.json');
+var injector = new di.Injector(injectables);
+
+var taskGraphRunner = injector.get('TaskGraph.Runner');
+var logger = injector.get('Logger').initialize('Tasks');
+var app = require('express')();
+var http = require('http');
+var swaggerTools = require('swagger-tools');
+var parseArgs = require('minimist');
+
+var httpPort = 9006;
+var server;
+
+var argv = parseArgs(process.argv.slice(2));
+
+var options = {
+    domain: argv.domain || argv.d,
+    httpPort: argv['http-port'] || argv.p || httpPort
+};
+
+taskGraphRunner.start(options)
+    .then(function() {
+        return injector.get('Views').load();
+    })
+    .then(function() {
+        logger.info('Task Runner Started.');
+        if(options.httpPort) {
+             // swaggerRouter configuration
+             var swaggerOptions = {
+                 swaggerUi: '/swagger.json',
+                 controllers: './api/rest',
+                 // Conditionally turn on stubs (mock mode)
+                 useStubs: process.env.NODE_ENV === 'development' ? true : false
+
+             };
+             // The Swagger document (require it, build it programmatically,
+             // fetch it from a URL, ...)
+             var swaggerDoc = require('./api/swagger.json');
+
+             // Initialize the Swagger middleware
+             swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+                 // Interpret Swagger resources and attach metadata to request -
+                 // must be first in swagger-tools middleware chain
+                 app.use(middleware.swaggerMetadata());
+
+                 // Validate Swagger requests
+                 app.use(middleware.swaggerValidator());
+
+                 // Route validated requests to appropriate controller
+                 app.use(middleware.swaggerRouter(swaggerOptions));
+
+                 // Serve the Swagger documents and Swagger UI
+                 app.use(middleware.swaggerUi());
+
+                 // Start the server
+                 var config = {
+                     hostname: '0.0.0.0',
+                     httpPort: options.httpPort
+                 };
+                 server = http.createServer(app);
+                 server.listen(config.httpPort, config.hostname, function () {
+                     logger.info('Your server is listening on port %d ', config.httpPort);
+                     logger.info('Swagger-ui is available on http://%s:%d/docs', config.hostname,
+                                                                                 config.httpPort);
+                 });
+             });
+        }
+    })
+    .catch(function(error) {
+        //NOTE(heckj): I'm unclear why this is on the console directly and not
+        // using a logger. Would expect this to be logger.critical(), but
+        // leaving as is because I don't know the implications.
+        console.error(error.message || error.details);
+        console.error(error.stack || error.rawStack);
+        //      logger.critical('Task Graph Runner Startup Error.', { error: error });
+
+        process.nextTick(function () {
+            process.exit(1);
+        });
+    });
+
+process.on('SIGINT', function () {
+    taskGraphRunner.stop()
+        .catch(function (error) {
+            logger.critical('Task Graph Runner Shutdown Error.', { error: error });
+        })
+        .finally(function () {
+            if (server) {
+                server.close();
+            }
+            process.nextTick(function () {
+                process.exit(1);
+            });
+        });
+});
+
+module.exports = {
+    injector: injector,
+    taskSchemas: taskSchemas
 };
